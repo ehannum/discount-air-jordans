c070e77e82c01e6bd076c5e9f475bdd1d9a04e76
Eric Hannum <eric.hannum@xpo.com>
2017-06-08 18:07:56 -0700

    [wip] blow it up or burn it down

--- server/server.js
+++ server/server.js
@@ -6,7 +6,24 @@ var http = require('http');
 var server = http.Server(app);
 var path = require('path');
 
-var fs = require('fs');
+// firebase initialization
+var firebase = require('firebase');
+var fKey = process.env.FIREBASE_KEY || null;
+var db = null;
+
+// -- FIREBASE DATABASE SETUP
+
+if (fKey) {
+  firebase.initializeApp({
+    serviceAccount: JSON.parse(fKey),
+    databaseURL: 'https://discount-air-jordans.firebaseio.com/'
+  });
+
+  db = firebase.database();
+  console.log('Connected and authenticated to Firebase.');
+} else {
+  console.log('Firebase authentication failure: No Private Key found.');
+}
 
 // -- SERVE STATIC FILES and JSON
 
@@ -14,6 +31,47 @@ app.use(express.static('public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// -- SHIT
+
+app.get('/trash', function (req, res) {
+  if (!db) {
+    console.log('Error: Firebase authentication failed');
+    res.send('Error: Firebase authentication failed');
+    return;
+  }
+
+  var ref = db.ref('posts');
+
+  ref.once('value', function (data) {
+    res.send(data.val());
+  });
+});
+
+app.post('/trash', function (req, res) {
+  if (!db) {
+    console.log('Error: Firebase authentication failed');
+    res.send('Error: Firebase authentication failed');
+    return;
+  }
+
+  var ref = db.ref('posts');
+
+  var postRef = ref.push();
+
+  postRef.set(
+    {
+      content: req.body.content
+    }, function (err) {
+      if (err) {
+        console.log('error:', err);
+        res.send('ERROR: ' + err);
+      } else {
+        res.send('I sure hope nobody BREAKS MY WEBSITE.');
+      }
+    }
+  );
+});
 
 // -- START SERVER
 
